a275fb12e58bcb2add3810d8b98071220439ce23
It's Sappy <57357216+mastrmind-dev@users.noreply.github.com>
2020-05-16 15:48:33 +0530

    Update FXMLDocumentController.java

--- src/librarymanagementsystem/ui/addBook/FXMLDocumentController.java
+++ src/librarymanagementsystem/ui/addBook/FXMLDocumentController.java
@@ -8,11 +8,17 @@ package librarymanagementsystem.ui.addBook;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import librarymanagementsystem.database.DatabaseHandler;
 
 /**
@@ -36,10 +42,13 @@ public class FXMLDocumentController implements Initializable {
     private JFXButton cancelButton;
 
     DatabaseHandler databaseHandler;
+    @FXML
+    private AnchorPane rootPane;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         databaseHandler = new DatabaseHandler();
+        checkData();
     }    
 
     @FXML
@@ -72,7 +81,6 @@ public class FXMLDocumentController implements Initializable {
                 "'" + bookPublisher + "',"+
                 "" + true + ""+
                 ")";
-        System.out.println(qu);
         if(databaseHandler.execAction(qu)){
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
             alert.setHeaderText(null);
@@ -88,6 +96,22 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private void cancel(ActionEvent event) {
+        //System.exit(0);
+        Stage stage = (Stage)rootPane.getScene().getWindow();
+        stage.close();
+    }
+
+    private void checkData() {
+        String qu = "SELECT title FROM BOOK";
+        ResultSet rs = databaseHandler.execQuery(qu);
+        try {
+            while(rs.next()){
+                String titlex = rs.getString("title");
+                System.out.println(titlex);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
